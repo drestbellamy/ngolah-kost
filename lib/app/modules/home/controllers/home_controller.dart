df3f074e47cec4f5ca860e29ef40401509ba27c0
drestbellamy <drztntazure23@gmail.com>
2026-03-29 22:34:54 +0700

    UI home

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,18 +1,58 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
+  final selectedIndex = 0.obs;
+
+  // Dashboard data
+  final totalKost = 8.obs;
+  final totalKamar = 64.obs;
+  final kamarKosong = 12.obs;
+  final totalPenghuni = 52.obs;
+  final tagihanBelumBayar = 8.obs;
+  final menungguVerifikasi = 3.obs;
+
   @override
   void onInit() {
     super.onInit();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void changeTab(int index) {
+    selectedIndex.value = index;
+    
+    switch (index) {
+      case 0:
+        // Already on home
+        break;
+      case 1:
+        Get.toNamed('/kost');
+        break;
+      case 2:
+        Get.toNamed('/penghuni');
+        break;
+      case 3:
+        Get.toNamed('/profil');
+        break;
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  void navigateToKelolaTagihan() {
+    Get.toNamed('/kelola-tagihan');
+  }
+
+  void navigateToKelolaPengumuman() {
+    Get.toNamed('/kelola-pengumuman');
+  }
+
+  void navigateToKelolaPeraturan() {
+    Get.toNamed('/kelola-peraturan');
+  }
+
+  void navigateToVerifikasi() {
+    // Navigate to verification page
+    Get.snackbar(
+      'Info',
+      'Navigasi ke halaman verifikasi pembayaran',
+      snackPosition: SnackPosition.BOTTOM,
+    );
   }
 }
---
+++ lib/app/modules/home/views/widgets/dashboard_card.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class DashboardCard extends StatelessWidget {
+  final IconData icon;
+  final String value;
+  final String label;
+  final Color iconBgColor;
+  final VoidCallback? onTap;
+
+  const DashboardCard({
+    super.key,
+    required this.icon,
+    required this.value,
+    required this.label,
+    required this.iconBgColor,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 165,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: const Color(0xFFF3F4F6),
+            width: 1.1,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 15,
+              offset: const Offset(0, 10),
+            ),
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 6,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                  color: iconBgColor.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Icon(
+                  icon,
+                  size: 28,
+                  color: iconBgColor,
+                ),
+              ),
+              const Spacer(),
+              Text(
+                value,
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF2F2F2F),
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(height: 4),
+              Text(
+                label,
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFF6B7280),
+                  height: 1.2,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
